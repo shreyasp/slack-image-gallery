bb2908b043abe813b2a43c969a985c2a722fc8af
Shreyas Patil <shreyasp@users.noreply.github.com>
2019-03-21 12:25:59 +0530

    Using bootstrap instead plain HTML

--- src/components/ImageCards.tsx
+++ src/components/ImageCards.tsx
@@ -1,6 +1,7 @@
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 export interface IProps {
     index: number;
@@ -13,18 +14,20 @@ export interface IProps {
 class ImageCards extends React.Component<IProps, {}> {
     render() {
         return(
-            <div key={this.props.index} className="w-1/5 max-w-sm m-2 rounded overflow-hidden shadow-md">
-                <img className="w-full" src={this.props.srcURI} alt={this.props.altText} />
-                <div className="relative px-3 py-4">
-                    <div className="absolute pin-r pin-t mt-1 mr-2">
-                        <FontAwesomeIcon icon={faHeart}/>
-                        <span className="ml-2">{this.props.likes || 0}</span>
-                    </div>
-                    <div className="absolute pin-l pin-t ml-2 mt-1 font-bold">
+            <Card className="w-1/5 m-2 shadow-lg rounded-lg">
+                <Card.Img className="rounded-t-lg rounded-b-none" variant="top" src={this.props.srcURI}/>
+                <Card.ImgOverlay className="relative px-4 py-4">
+                    <Card.Text className="absolute pin-l pin-t font-bold m-2 text-base">
                         {this.props.caption}
-                    </div>
-                </div>
-            </div>
+                    </Card.Text>
+                    <Card.Text>
+                        <div className="absolute pin-r pin-t m-2">
+                            <FontAwesomeIcon icon={faHeart}/>
+                            <span className="ml-2">{this.props.likes || 0}</span>
+                        </div>
+                    </Card.Text>
+                </Card.ImgOverlay>
+            </Card>
         );
     }
 }
